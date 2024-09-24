d4e75eb5b1e2b695495586db0626de11301e798e
Stev <stevenvallejo780@gmail.com>
2024-09-23 21:42:45 -0500

    Actualizar rutas de imágenes y metadatos en página principal

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,3 @@
-import Head from 'next/head';
 import Header from './components/Header';
 import Tools from './components/Tools';
 import Portfolio from './components/Portafolio';
@@ -10,19 +9,6 @@ import Script from 'next/script';
 export default function Home() {
   return (
     <>
-      <Head>
-        <title>Steven Vallejo Ortiz - Hoja de Vida</title>
-        <meta name="description" content="Soy Steven Vallejo Ortiz, científico de la computación, ingeniero de software, emergentista y holista, con pasión por la filosofía y las ciencias." />
-        <meta property="og:title" content="Steven Vallejo Ortiz - Hoja de Vida" />
-        <meta property="og:description" content="Descubre más sobre mi experiencia, habilidades y logros. Estoy comprometido a ayudar a la humanidad a través del pensamiento crítico y soluciones tecnológicas." />
-        <meta property="og:image" content="/images/profile.jpeg" />
-        <meta property="og:url" content="https://stevenvallejo.com" />
-        <meta name="twitter:card" content="summary_large_image" />
-        <meta property="og:type" content="website" />
-        <meta charSet="UTF-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-      </Head>
-
       <Script
         strategy="afterInteractive"
         src={`https://www.googletagmanager.com/gtag/js?id=G-E5NMYWLXER`}
--- src/app/pages/_app.tsx
+++ src/app/pages/_app.tsx
@@ -1,12 +1,28 @@
 import { AppProps } from 'next/app';
 import Layout from '@/app/layout';
+import Head from 'next/head';
 
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <>
+      <Head>
+        <title>Steven Vallejo Ortiz - Hoja de Vida</title>
+        <meta name="description" content="Soy Steven Vallejo Ortiz, científico de la computación, ingeniero de software, emergentista y holista, con pasión por la filosofía y las ciencias." />
+        <meta property="og:title" content="Steven Vallejo Ortiz - Hoja de Vida" />
+        <meta property="og:description" content="Descubre más sobre mi experiencia, habilidades y logros. Estoy comprometido a ayudar a la humanidad a través del pensamiento crítico y soluciones tecnológicas." />
+        <meta property="og:image" content="/images/profile.jpeg" />
+        <meta property="og:url" content="https://stevenvallejo.com" />
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta property="og:type" content="website" />
+        <meta charSet="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      </Head>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </>
+
   );
 }
 
