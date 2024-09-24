0180fdb8fe88acdb1af64d24d7426237fc72f3bf
Stev <stevenvallejo780@gmail.com>
2024-09-23 21:39:08 -0500

    Actualizar rutas de imágenes y metadatos en página principal

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,6 +5,7 @@ import Portfolio from './components/Portafolio';
 import Skills from './components/Skills';
 import Achievements from './components/Achievements';
 import ContactMe from './components/ContactMe';
+import Script from 'next/script';
 
 export default function Home() {
   return (
@@ -21,13 +22,42 @@ export default function Home() {
         <meta charSet="UTF-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       </Head>
-      
+
+      <Script
+        strategy="afterInteractive"
+        src={`https://www.googletagmanager.com/gtag/js?id=G-E5NMYWLXER`}
+      />
+      <Script
+        id="google-analytics"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-E5NMYWLXER');
+          `,
+        }}
+      />
       <Header />
-      <Achievements />
-      <Portfolio />
-      <Tools />
-      <Skills />
-      <ContactMe />
+      <main>
+        <section>
+          <Achievements />
+        </section>
+        <section>
+          <Portfolio />
+        </section>
+        <section>
+          <Tools />
+        </section>
+        <section>
+          <Skills />
+        </section>
+        <section>
+          <ContactMe />
+        </section>
+      </main>
+      
     </>
   );
 }
